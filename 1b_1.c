41fa6141d7eae4661f7e1c13d93a6125daf5d5d6
fabio <pozzi.fabio@gmail.com>
2018-08-01 09:21:50 +0200

    Esercizio 1b_1 (rettifica).

--- 1b_1.c
+++ 1b_1.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
+void leggi_vettore(int vet[], int n);
 void stampa_vettore(int vet[], int n);
 
 int main(void)
 {
-    int vet[5] = {2, 3, 5, 3, 6};
+    int vet[5];
+    leggi_vettore(vet, 5);
     stampa_vettore(vet, 5);
+}
+
+void leggi_vettore(int vet[], int n)
+{
+    for (int i=0; i < n; i++) {
+        printf("Inserisci valore posizione %d: ", i);
+        scanf("%d", &vet[i]);
+    }
+    return;
 }
 
 void stampa_vettore(int vet[], int n)
 {
+    int i;
     printf("indici:\t");
-    for (int i=0; i < n; i++) {
+    for (i=0; i < n; i++) {
         printf("%d\t", i);
     }
     printf("\nvalori:\t");
-    for (int i=0; i < n; i++) {
+    for (i=0; i < n; i++) {
         printf("%d\t", vet[i]);
     }
     printf("\n");
